f297579347bdafe98f5367cabdcb576f9e32ce7a
NiponNumb <oropeza012@hotmail.com>
2023-09-19 19:45:48 -0600

    React App develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useRef } from 'react';
 import { add, divide, multiply, subtract } from './utils/math';
+import Todo from './components/List';
 
 function App() {
-  const [counter, setCounter] = useState(2);
+  /*const [counter, setCounter] = useState(2);
   const [result, setResult] = useState(0);
   // const inputARef = document.querySelector('#a');
   const inputARef = useRef(null);
@@ -42,35 +43,11 @@ function App() {
     const a = Number(inputARef.current.value);
     const b = Number(inputBRef.current.value);
     setResult(subtract(a, b));
-  };
-
-  return (
+  };*/
+   return (
     <div className="container">
-      <h1>Counter: {counter}</h1>
-      <button type="button" onClick={handleCounter}>
-        count
-      </button>
-
-      <div>
-        <h1>Calculator</h1>
-        <input type="number" name="a" ref={inputARef} />
-        <input type="number" name="b" ref={inputBRef} />
-        <button type="button" onClick={handleAdd}>
-          Add
-        </button>
-        <button type="button" onClick={handleMultiply}>
-          Multiply
-        </button>
-        <button type="button" onClick={handleDivide}>
-          Divide
-        </button>
-        <button type="button" onClick={handleSubtract}>
-          Subtract
-        </button>
-        <h2>Result: {result}</h2>
-      </div>
-    </div>
-  );
+    <Todo />
+  </div>
+   )
 }
-
 export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from 'react';
+
+function Todo() {
+  const inputRef = useRef(null);
+  const [tasks, setTasks] = useState([]); 
+  const [editingIndex, setEditingIndex] = useState(null); 
+  const [editingText, setEditingText] = useState(""); 
+
+  const addTask = () => {
+    const taskText = inputRef.current.value;
+    if (taskText) {
+      setTasks([...tasks, taskText]);
+      inputRef.current.value = "";
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (index) => {
+    setEditingIndex(index);
+    setEditingText(tasks[index]);
+  };
+
+  const saveTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = editingText;
+    setTasks(updatedTasks);
+    setEditingIndex(null); 
+  };
+
+  return (
+    <div>
+      <h1 style={{color:"yellow"}}>Lista por hacer</h1>
+      <div className="container" style={{ display: "flex", gap: "1rem"}}>
+        <input type="text" ref={inputRef} />
+        <button onClick={addTask} style={{color:"white"}}>Agregar</button>
+      </div>
+      <div>
+        <h2 style={{color:"yellow"}}>Lista</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {editingIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={editingText}
+                    onChange={(e) => setEditingText(e.target.value)}
+                  />
+                  <button onClick={() => saveTask(index)} style={{color:"blue"}}>Guardar</button>
+                </>
+              ) : (
+                <>
+                  {task}
+                  <button onClick={() => deleteTask(index)} style={{color:"red"}}>Eliminar</button>
+                  <button onClick={() => editTask(index)} style={{color:"aliceblue"}}>Actualizar</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
