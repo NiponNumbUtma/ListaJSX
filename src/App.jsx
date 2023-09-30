1aa113e44e1049d96148e35cefc27ef827eca05f
NiponNumbUtma <utm20040043@utma.edu.mx>
2023-09-29 20:25:03 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,6 @@ import { add, divide, multiply, subtract } from './utils/math';
 import Todo from './components/List';
 
 function App() {
-  /*const [counter, setCounter] = useState(2);
-  const [result, setResult] = useState(0);
-  // const inputARef = document.querySelector('#a');
-  const inputARef = useRef(null);
-  const inputBRef = useRef(null);
-
-  const handleCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleAdd = () => {
-    // const a = intpuAref.value;
-    console.log(inputARef.current);
-    const a = Number(inputARef.current.value);
-    const b = Number(inputBRef.current.value);
-    setResult(add(a, b));
-  };
-
-  const handleMultiply = () => {
-    // const a = intpuAref.value;
-    console.log(inputARef.current);
-    const a = Number(inputARef.current.value);
-    const b = Number(inputBRef.current.value);
-    setResult(multiply(a, b));
-  };
-
-  const handleDivide = () => {
-    // const a = intpuAref.value;
-    console.log(inputARef.current);
-    const a = Number(inputARef.current.value);
-    const b = Number(inputBRef.current.value);
-    setResult(divide(a, b));
-  };
-
-  const handleSubtract = () => {
-    // const a = intpuAref.value;
-    console.log(inputARef.current);
-    const a = Number(inputARef.current.value);
-    const b = Number(inputBRef.current.value);
-    setResult(subtract(a, b));
-  };*/
    return (
     <div className="container">
     <Todo />
